0c113c9b7c283d2cd2e095f31afa0feffa8d6c35
julianperez12 <julianperez12@github.com>
2025-05-26 18:31:47 -0500

    agregando ms usuarios api gateway

--- src/main/java/com/project/gateway/gatewayms/GatewayApplication.java
+++ src/main/java/com/project/gateway/gatewayms/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.prject.gateway.gateway;
+package com.project.gateway.gatewayms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
