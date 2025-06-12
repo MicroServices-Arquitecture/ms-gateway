38ae73db54d12dff6abbe666a55b12fa7a6a16d1
william0814 <king-master90@hotmail.com>
2025-06-12 20:46:46 +0200

    Add acces to gateway to the FronEnd

--- src/main/java/com/project/gateway/gatewayms/GatewayApplication.java
+++ src/main/java/com/project/gateway/gatewayms/GatewayApplication.java
@@ -3,6 +3,18 @@ package com.project.gateway.gatewayms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +22,27 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebFilter corsFilter(){
+		return (ServerWebExchange exchange, WebFilterChain chain) ->{
+			ServerHttpRequest request = exchange.getRequest();
+			if (CorsUtils.isCorsRequest(request)){
+				ServerHttpResponse response = exchange.getResponse();
+				HttpHeaders headers = response.getHeaders();
+				headers.add("Access-Control-Allow-Origin", "http://localhost:5173");
+				headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+				headers.add("Access-Control-Allow-Headers", "*");
+				headers.add("Access-Control-Allow-Credentials", "true");
+
+				if (request.getMethod() == HttpMethod.OPTIONS){
+					response.setStatusCode(HttpStatus.OK);
+					return response.setComplete();
+				}
+			}
+			return chain.filter(exchange);
+		};
 	}
 
 }
